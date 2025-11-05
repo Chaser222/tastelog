65304400edc6b3a3c8f01eb37df70a1796d4bdc5
Chaser222 <kees.kollerie@gmail.com>
2025-11-05 19:34:06 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'tastelog-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+  })());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(e.request, { ignoreSearch: true });
+    if (cached) return cached;
+    try {
+      const res = await fetch(e.request);
+      if (res && res.ok && new URL(e.request.url).origin === location.origin) {
+        cache.put(e.request, res.clone());
+      }
+      return res;
+    } catch {
+      return cached || Response.error();
+    }
+  })());
+});
